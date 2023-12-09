455f794b2fc0f1e9eadc973f4b27b7fa2acb4556
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-12-10 00:24:47 +0330

    create boxes section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css"
 import Header from "./components/Header/Header"
 import Landing from "./components/Landing/Landing"
+import Box from "./components/Box/Box"
+
 function App() {
   return (
     <div className="">
       <section className="text-white">
         <Header />
         <Landing />
+      </section>
+      <section className="container-Primary flex justify-between gap-5 mt-10">
+        <Box title="طراحی خلاقانه" icon="FaMagic" />
+        <Box title="پشتیبانی رایگان" icon="FaRocket" />
+        <Box title="طراحی اختصاصی" icon="IoDiamondOutline" />
       </section>
     </div>
   )
